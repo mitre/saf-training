5214db2d07660c50bae44cca46d198bc47947521
Amndeep Singh Mann <amann@mitre.org>
2026-09-16 22:40:39 -0400

    update cypress config to get rid of unused stuff and gitignore to ignore cypress outputs
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://127.0.0.1:8080',
   },
 });
