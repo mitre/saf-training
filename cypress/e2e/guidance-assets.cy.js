58bdb47a2b4a2a798030a4d27ec0fb745563717a
Amndeep Singh Mann <amann@mitre.org>
2026-09-16 23:06:39 -0400

    make checks more rigorous here, ex. the scroll into view means that the image has to be rendered on page
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- cypress/e2e/guidance-assets.cy.js
+++ cypress/e2e/guidance-assets.cy.js
@@ -1,17 +1,26 @@
+import { visitPage } from '../support/visit-page';
+
 describe('Guidance course assets and links', () => {
   it('loads a bundled image', () => {
-    cy.visit('/courses/guidance/08.html');
-    cy.get('img[alt="Related Rules Button"]')
-      .scrollIntoView()
+    visitPage('/courses/guidance/08.html');
+    cy.get('main img[alt="Related Rules Button"]').scrollIntoView();
+    cy.get('main img[alt="Related Rules Button"]')
       .should('be.visible')
       .and(($image) => {
+        expect($image).to.have.length(1);
+        expect($image[0].complete).to.equal(true);
         expect($image[0].naturalWidth).to.be.greaterThan(0);
+        expect($image[0].naturalHeight).to.be.greaterThan(0);
       });
   });
 
   it('links to the beginner course', () => {
-    cy.visit('/courses/guidance/09.html');
-    cy.contains('a', 'training class').click();
+    visitPage('/courses/guidance/09.html');
+    cy.contains('main a', 'training class')
+      .should('have.attr', 'href', '/courses/beginner/');
+    cy.contains('main a', 'training class').click();
     cy.location('pathname').should('eq', '/courses/beginner/');
+    cy.get('main h1').should('be.visible')
+      .and('contain.text', 'Beginner Security Automation Developer Class');
   });
 });
